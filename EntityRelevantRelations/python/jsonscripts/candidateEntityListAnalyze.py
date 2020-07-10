269280bf70882ed48705fa7434e7afcda4e344b5
poojahoza <oza.pooja.h@gmail.com>
2020-07-09 21:21:59 -0400

    Python script to check the existence of qrel entities in candidate entity list. Change - 8

--- EntityRelevantRelations/python/jsonscripts/candidateEntityListAnalyze.py
+++ EntityRelevantRelations/python/jsonscripts/candidateEntityListAnalyze.py
@@ -7,7 +7,7 @@ def process_json_files(input_json_dir):
     query_list = dict()
     print(len(files))
     for file in files:
-        with open(file,'r',encoding='utf-8') as f:
+        with open(input_json_dir+file,'r',encoding='utf-8') as f:
             print(os.path.abspath(file))
             json_decode = json.load(f)
             print(len(json_decode))
