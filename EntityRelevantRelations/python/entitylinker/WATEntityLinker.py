b7a3a96298741f751df93e2bd2bbbd27273d17ff
poojahoza <oza.pooja.h@gmail.com>
2020-07-11 11:43:29 -0400

    WAT Entity Linker python script - change 3

--- EntityRelevantRelations/python/entitylinker/WATEntityLinker.py
+++ EntityRelevantRelations/python/entitylinker/WATEntityLinker.py
@@ -28,7 +28,7 @@ class WATAnnotation:
 def wat_entity_linking(text):
 
     wat_url='https://wat.d4science.org/wat/tag/tag'
-    payload=[("gcub_token",MY_GCUBE_TOKEN),
+    payload=[("gcub-token",MY_GCUBE_TOKEN),
              ("text",text),
              ("lang","en"),
              ("tokenizer","nlp4j"),
