ac4076f6f9f5c55bd4656b3d2b3a66e14ce121ff
poojahoza <oza.pooja.h@gmail.com>
2020-07-11 10:09:22 -0400

    Scripts to fetch WAT annotations from the json file.

---
+++ EntityRelevantRelations/python/entitylinker/WATEntityLinkerWrapper.py
@@ -0,0 +1,28 @@
+import json
+import WATEntityLinker
+
+def writeToFile(output_file, final_dict):
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(output_file, final_dict)
+
+def fetchFilesFromFolder(input_file):
+    final_json = []
+    with open(input_file,'r',encoding='utf-8') as f:
+        json_decode = json.load(f)
+        for item in json_decode:
+            temp_item = dict()
+            wat_annotations = WATEntityLinker.wat_entity_linking(item['contexttext'])
+            temp_item['queryid'] = item['queryid']
+            temp_item['contexttext'] = item['contexttext']
+            temp_item['contextid'] = item['contextid']
+            temp_item['contextrank'] = item['contextrank']
+            temp_item['contextscore'] = item['contextscore']
+            temp_item['WATannotations'] = wat_annotations
+            final_json.append(temp_item)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Please provide input file and output file location")
+    parser.add_argument("--i",help="Input JSON file location")
+    parser.add_argument("--o",help="Output JSON file location")
+    final_json = fetchFilesFromFolder(args.i)
+    writeToFile(args.o, final_json)
