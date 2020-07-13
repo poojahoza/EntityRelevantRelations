c59053625089649025b405c815d07d8be55d1ea0
poojahoza <oza.pooja.h@gmail.com>
2020-07-13 13:14:50 -0400

    Changed wat annotations objects to json dict to make the json serializable.

--- EntityRelevantRelations/python/entitylinker/WATEntityLinkerWrapper.py
+++ EntityRelevantRelations/python/entitylinker/WATEntityLinkerWrapper.py
@@ -15,12 +15,13 @@ def fetchFilesFromFolder(input_file):
             temp_item = dict()
             print(counter)
             wat_annotations = WATEntityLinker.wat_entity_linking(item['contexttext'])
+            wat_json_list = [w.json_dict() for w in wat_annotations]
             temp_item['queryid'] = item['queryid']
             temp_item['contexttext'] = item['contexttext']
             temp_item['contextid'] = item['contextid']
             temp_item['contextrank'] = item['contextrank']
             temp_item['contextscore'] = item['contextscore']
-            temp_item['WATannotations'] = wat_annotations
+            temp_item['WATannotations'] = wat_json_list
             final_json.append(temp_item)
             # temp condition remove it
             if counter >= 6:
