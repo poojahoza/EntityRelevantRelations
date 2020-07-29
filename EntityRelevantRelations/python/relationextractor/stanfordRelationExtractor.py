594911fee989bc60e1ba910c21a1297896237edf
poojahoza <oza.pooja.h@gmail.com>
2020-07-29 15:25:31 -0400

    Added AnnotationException try catch and error log in stanfordRelationExtractor.py

--- EntityRelevantRelations/python/relationextractor/stanfordRelationExtractor.py
+++ EntityRelevantRelations/python/relationextractor/stanfordRelationExtractor.py
@@ -4,6 +4,10 @@ import os
 import sys
 
 from stanfordnlp.server import CoreNLPClient
+from stanfordnlp.server.client import AnnotationException
+
+from utils import readwriteutils
+
 
 def check_file_existence(output_file_name):
     if os.path.exists(output_file_name):
@@ -33,12 +37,12 @@ def build_triples_charoffset_list(tokens_dict, sentence_json, annotated_text):
                     break
     return relation_tokens
 
+
 def extract_relations(input1, coref_flag):
-    input_json = dict()
     output_json = []
-    with open(input1,'r') as f:
-        input_json=json.load(f)
-    with CoreNLPClient(properties={'annotators':'tokenize,ssplit,pos,lemma,ner,depparse,natlog, coref, openie','openie.resolve_coref':coref_flag},timeout=15000,memory='16G') as client:
+    input_json = readwriteutils.read_json_file(input1)
+    error_log = []
+    with CoreNLPClient(properties={'annotators': 'tokenize,ssplit,pos,lemma,ner,depparse,natlog, coref, openie','openie.resolve_coref': coref_flag}, timeout=30000, memory='16G') as client:
         counter = 1
         for query in input_json:
             query_json = dict()
@@ -48,49 +52,55 @@ def extract_relations(input1, coref_flag):
             query_json['contexttext'] = query['contexttext']
             query_json['contextrank'] = query['contextrank']
             query_json['contextscore'] = query['contextscore']
-            annotated_text = client.annotate(query['contexttext'])
+            try:
+                annotated_text = client.annotate(query['contexttext'])
 
-            relation_triples_list = []
-            for s in annotated_text.sentence:
-                token_json = dict()
-                for tk in s.token:
-                    tk_details = dict()
-                    tk_details['token'] = tk.word
-                    tk_details['charOffsetBegin'] = str(tk.beginChar)
-                    tk_details['charOffsetEnd'] = str(tk.endChar)
-                    token_json[tk.tokenBeginIndex] = tk_details
-                sentence_json[str(s.sentenceIndex)] = token_json
-                for triple in s.openieTriple:
-                    triple_json = dict()
-                    triple_json['subject'] = triple.subject
-                    triple_json['relation'] = triple.relation
-                    triple_json['object'] = triple.object
-                    triple_json['subjectTokens'] = build_triples_charoffset_list(triple.subjectTokens, sentence_json, annotated_text)
-                    triple_json['relationTokens'] = build_triples_charoffset_list(triple.relationTokens, sentence_json, annotated_text)
-                    triple_json['objectTokens'] = build_triples_charoffset_list(triple.objectTokens, sentence_json, annotated_text)
-                    relation_triples_list.append(triple_json)
-                query_json['relationTriples'] = relation_triples_list
+                relation_triples_list = []
+                for s in annotated_text.sentence:
+                    token_json = dict()
+                    for tk in s.token:
+                        tk_details = dict()
+                        tk_details['token'] = tk.word
+                        tk_details['charOffsetBegin'] = str(tk.beginChar)
+                        tk_details['charOffsetEnd'] = str(tk.endChar)
+                        token_json[tk.tokenBeginIndex] = tk_details
+                    sentence_json[str(s.sentenceIndex)] = token_json
+                    for triple in s.openieTriple:
+                        triple_json = dict()
+                        triple_json['subject'] = triple.subject
+                        triple_json['relation'] = triple.relation
+                        triple_json['object'] = triple.object
+                        triple_json['subjectTokens'] = build_triples_charoffset_list(triple.subjectTokens, sentence_json, annotated_text)
+                        triple_json['relationTokens'] = build_triples_charoffset_list(triple.relationTokens, sentence_json, annotated_text)
+                        triple_json['objectTokens'] = build_triples_charoffset_list(triple.objectTokens, sentence_json, annotated_text)
+                        relation_triples_list.append(triple_json)
+                    query_json['relationTriples'] = relation_triples_list
+            except AnnotationException as ae:
+                error_log.append('%s %s %s %s' % query['queryid'], query['contextid'], 'AnnotationException', str(ae))
+                query_json['relationTriples'] = []
             output_json.append(query_json)
             print(counter)
             counter = counter + 1
-            if counter == 2:
-                break
-    return output_json
+    return output_json, error_log
+
 
 def write_file(output_file, output_dict):
     check_file_existence(output_file)
-    with open(output_file,'w',encoding='utf-8') as f1:
-        json.dump(output_dict,f1)
+    with open(output_file, 'w', encoding='utf-8') as f1:
+        json.dump(output_dict, f1)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-            "Please provide input json file and output json file location")
+        "Please provide input json file and output json file location")
     parser.add_argument('--i', help='input json file location')
-    parser.add_argument('--o',help='output json file location')
-    parser.add_argument('--corefflag',help='coref flag')
+    parser.add_argument('--o', help='output json file location')
+    parser.add_argument('--corefflag', help='coref flag')
+    parser.add_argument('--e', help='error log file')
     args = parser.parse_args()
     if len(sys.argv) == 1:
-            parser.print_help(sys.stderr)
-            sys.exit(1)
-    extracted_relations = extract_relations(args.i, args.corefflag)
-    write_file(args.o, extracted_relations)
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    extracted_relations, error_logs = extract_relations(args.i, args.corefflag)
+    readwriteutils.write_json_file(args.o, extracted_relations)
+    readwriteutils.write_text_file(args.e, error_logs)
---
+++ EntityRelevantRelations/python/utils/readwriteutils.py
@@ -0,0 +1,47 @@
+import json
+
+from json.decoder import JSONDecodeError
+
+
+def read_json_file(file_location):
+    try:
+        with open(file_location, 'r', encoding='utf-8') as f:
+            json_dict = json.load(f)
+        return json_dict
+    except JSONDecodeError as e:
+        print(e)
+        return None
+
+
+def write_json_file(file_location, output_json):
+    try:
+        with open(file_location, 'w', encoding='utf-8') as f:
+            json.dump(output_json, f)
+            return True
+    except TypeError as t:
+        print(t)
+        return False
+
+
+def read_converted_entity_ids(file_location):
+    converted_entiy_ids = dict()
+    try:
+        with open(file_location, 'w', encoding='utf-8') as f:
+            for line in f:
+                data = line.split('\t')
+                entity_ids_list = []
+                if data[1] in converted_entiy_ids:
+                    entity_ids_list = converted_entiy_ids[data[1]]
+
+                entity_ids_list.append(data[0])
+                converted_entiy_ids[data[1]] = entity_ids_list
+        return converted_entiy_ids
+    except Exception as e:
+        print(e)
+        return None
+
+
+def write_text_file(file_location, output_list):
+    with open(file_location, 'w', encoding='utf-8') as f:
+        for line in output_list:
+            f.write('%s\n' % line)
