40a8228ce0474059d0fb58c53ef15380cc472026
poojahoza <oza.pooja.h@gmail.com>
2020-07-30 17:27:16 -0400

    changed readwriteutils.py - read_converted_entity_ids to read from folder

--- EntityRelevantRelations/python/utils/readwriteutils.py
+++ EntityRelevantRelations/python/utils/readwriteutils.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from json.decoder import JSONDecodeError
 
@@ -23,18 +24,20 @@ def write_json_file(file_location, output_json):
         return False
 
 
-def read_converted_entity_ids(file_location):
+def read_converted_entity_ids(folder_location):
+    files = os.listdir(folder_location)
     converted_entiy_ids = dict()
     try:
-        with open(file_location, 'w', encoding='utf-8') as f:
-            for line in f:
-                data = line.split('\t')
-                entity_ids_list = []
-                if data[1] in converted_entiy_ids:
-                    entity_ids_list = converted_entiy_ids[data[1]]
+        for file in files:
+            with open(folder_location+file, 'r', encoding='utf-8') as f:
+                for line in f:
+                    data = line.split('\t')
+                    entity_ids_list = []
+                    if data[1] in converted_entiy_ids:
+                        entity_ids_list = converted_entiy_ids[data[1]]
 
-                entity_ids_list.append(data[0])
-                converted_entiy_ids[data[1]] = entity_ids_list
+                    entity_ids_list.append(data[0])
+                    converted_entiy_ids[data[1]] = entity_ids_list
         return converted_entiy_ids
     except Exception as e:
         print(e)
