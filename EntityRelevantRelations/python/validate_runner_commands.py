5c0b9f255f2d310e6d0084a19d4b616e1dc12ce3
poojahoza <oza.pooja.h@gmail.com>
2020-08-08 22:48:27 -0400

    validate_runner_commands.py - changed logic

--- EntityRelevantRelations/python/validate_runner_commands.py
+++ EntityRelevantRelations/python/validate_runner_commands.py
@@ -1,26 +1,26 @@
 def validate_watlinker(parser_args):
-    if not ('input' in parser_args and 'output' in parser_args):
-        return False
-    else:
+    if 'input' in parser_args and 'output' in parser_args:
         return True
+    else:
+        return False
 
 
 def validate_stanford(parser_args):
-    if not ('input' in parser_args and 'output' in parser_args and 'corefflag' in parser_args and 'errorlog' in parser_args):
-        return False
-    else:
+    if 'input' in parser_args and 'output' in parser_args and 'corefflag' in parser_args and 'errorlog' in parser_args:
         return True
+    else:
+        return False
 
 
 def validate_entityfreq(parser_args):
-    if not ('annotations' in parser_args and 'field' in parser_args and 'limit' in parser_args and 'output' in parser_args):
-        return False
-    else:
+    if 'annotations' in parser_args and 'field' in parser_args and 'limit' in parser_args and 'output' in parser_args:
         return True
+    else:
+        return False
 
 
 def validate_ranklib(parser_args):
-    if not ('qrel' in parser_args and 'feature' in parser_args and 'zscore' in parser_args and 'output' in parser_args and 'outputzscore' in parser_args and 'rankliboutput' in parser_args):
-        return False
-    else:
+    if 'qrel' in parser_args and 'feature' in parser_args and 'zscore' in parser_args and 'output' in parser_args and 'outputzscore' in parser_args and 'rankliboutput' in parser_args:
         return True
+    else:
+        return False
