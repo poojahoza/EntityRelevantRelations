3a927828f87acac27e66a2e40cc0c0c9265f2eb6
poojahoza <oza.pooja.h@gmail.com>
2020-08-27 11:00:16 -0400

    convert_relationsann_to_WATann_schema_with_filtered_relations.py - convert relation annotations to WAT ann schema with filtered relations

---
+++ EntityRelevantRelations/python/jsonscripts/convert_relationsann_to_WATannotations_schema_with_filtered_relations.py
@@ -0,0 +1,51 @@
+import json
+import argparse
+
+
+def convert_relation_annotations_to_wat_annotations_schema_with_flitered_relations(input_file, output_file):
+
+    final_output_json = []
+    with open(input_file, 'r') as f:
+        input_json = json.load(f)
+    for item in input_json:
+        item_dict = dict()
+        item_dict['queryid'] = item['queryid']
+        item_dict['contextid'] = item['contextid']
+        item_dict['contexttext'] = item['contexttext']
+        item_dict['contextrank'] = item['contextrank']
+        item_dict['contextscore'] = item['contextscore']
+        rel_annotations = []
+        for rel in item['relAnnotations']:
+            sub_counter = 0
+            obj_counter = 0
+            for sub in rel['subjectAnnotations']:
+                for entityid in sub['wiki_converted_id']:
+                    sub_counter = sub_counter + 1
+                    # rel_annotations.append(entityid)
+            for obj in rel['objectAnnotations']:
+                for ent in obj['wiki_converted_id']:
+                    obj_counter = obj_counter + 1
+                    # rel_annotations.append(ent)
+
+            if sub_counter > 0 and obj_counter > 0:
+                for sub in rel['subjectAnnotations']:
+                    for entityid in sub['wiki_converted_id']:
+                        rel_annotations.append(entityid)
+                for obj in rel['objectAnnotations']:
+                    for ent in obj['wiki_converted_id']:
+                        # obj_counter = obj_counter + 1
+                        rel_annotations.append(ent)
+
+        item_dict['WATEntitiesTitle'] = rel_annotations
+        final_output_json.append(item_dict)
+
+    with open(output_file, 'w', encoding='utf-8') as f1:
+        json.dump(final_output_json, f1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Please provide input para aggr jsonl file and output file path")
+    parser.add_argument("--i", help="Input relation annotation JSON file location")
+    parser.add_argument("--o", help="Output JSON file location")
+    args = parser.parse_args()
+    convert_relation_annotations_to_wat_annotations_schema_with_flitered_relations(args.i, args.o)
