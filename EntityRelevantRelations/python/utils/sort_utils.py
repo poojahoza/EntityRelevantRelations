f8238eea19b1201e0f7805842d87cad6ee618d47
poojahoza <oza.pooja.h@gmail.com>
2020-08-28 10:50:52 -0400

    relations_freq.py - added a option of calculating frequency of all candidate set of entities.

--- EntityRelevantRelations/python/utils/sort_utils.py
+++ EntityRelevantRelations/python/utils/sort_utils.py
@@ -10,3 +10,11 @@ def sort_elements(inputjson, limit):
         sorted_dict = heapq.nlargest(limit, val.items(), key=itemgetter(1))
         sortedinputjson[key] = dict(sorted_dict)
     return sortedinputjson
+
+
+def sort_elements_by_value(inputjson):
+
+    sortedjson = dict()
+    for key, value in inputjson.items():
+        sortedjson[key] = {k:v for k, v in sorted(value.items(), key=lambda item: item[1])}
+    return sortedjson
