af499c9bc045936b91e71308f3f8eea6acc61c61
poojahoza <oza.pooja.h@gmail.com>
2020-08-30 17:28:30 -0400

    convert_annotations_to_WATannotations_schema.py - changed logic to list extend

--- EntityRelevantRelations/python/jsonscripts/convert_annotations_to_WATannotations_schema.py
+++ EntityRelevantRelations/python/jsonscripts/convert_annotations_to_WATannotations_schema.py
@@ -45,7 +45,7 @@ def convert_annotations_to_wat_annotations_schema(input_file_loc, entity_id_conv
         wat_annotations = []
         for ann in item['WATannotations']:
             if ann['wiki_title'] in converted_entity_ids:
-                wat_annotations.append(converted_entity_ids[ann['wiki_title']])
+                wat_annotations.extend(converted_entity_ids[ann['wiki_title']])
 
         item_dict['WATEntitiesTitle'] = wat_annotations
         final_output_json.append(item_dict)
