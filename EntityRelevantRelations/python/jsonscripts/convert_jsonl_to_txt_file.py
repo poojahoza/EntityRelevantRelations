bb5e079b9eab533d02cf03874e82c21c93103fa2
poojahoza <oza.pooja.h@gmail.com>
2020-09-07 12:58:41 -0400

    added python script to convert jsonl to text file

---
+++ EntityRelevantRelations/python/jsonscripts/convert_jsonl_to_txt_file.py
@@ -0,0 +1,21 @@
+import jsonlines
+import argparse
+
+
+def generate_trec_eval_run_file_from_jsonl(input_file_loc, output_file_loc, field):
+    text_file_list = []
+    with jsonlines.open(input_file_loc, "r") as f:
+        for line in f:
+            text_file_list.append(line["query"]+" Q0 "+line["document"][field]+" "+line["rank"]+" "+line["score"]+" unh_trema "+line["method"])
+
+    with open(output_file_loc, "w", encoding='utf-8') as writer:
+        for l in text_file_list:
+            writer.write('%s\n' % l)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Please provide input jsonl file and output txt file location")
+    parser.add_argument('--r', help='input-runs folder location')
+    parser.add_argument('--o', help='output text file location')
+    parser.add_argument('--f', help='values: paragraph|entity')
+    args = parser.parse_args()
+    run_data = generate_trec_eval_run_file_from_jsonl(args.r, args.o, args.f)
