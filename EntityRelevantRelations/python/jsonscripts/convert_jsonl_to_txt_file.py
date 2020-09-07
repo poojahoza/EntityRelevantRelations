82f5377ef157c446f79a4bd5b7c775139f4275d9
poojahoza <oza.pooja.h@gmail.com>
2020-09-07 13:24:29 -0400

    modified convert_jsonl_to_txt_file.py

--- EntityRelevantRelations/python/jsonscripts/convert_jsonl_to_txt_file.py
+++ EntityRelevantRelations/python/jsonscripts/convert_jsonl_to_txt_file.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Please provide input jsonl file and output txt file location")
     parser.add_argument('--r', help='input-runs folder location')
     parser.add_argument('--o', help='output text file location')
-    parser.add_argument('--f', help='values: paragraph|entity')
+    parser.add_argument('--f', help='values: paragraph|entity|neighbor')
     args = parser.parse_args()
     run_data = generate_trec_eval_run_file_from_jsonl(args.r, args.o, args.f)
