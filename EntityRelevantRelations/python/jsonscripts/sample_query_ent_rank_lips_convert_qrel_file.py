768a660cec56cc3ec502c980ef12923049c3da16
poojahoza <oza.pooja.h@gmail.com>
2020-09-08 16:50:25 -0400

    sample_query_ent_rank_lips_convert_qrel_file.py - updated

--- EntityRelevantRelations/python/jsonscripts/sample_query_ent_rank_lips_convert_qrel_file.py
+++ EntityRelevantRelations/python/jsonscripts/sample_query_ent_rank_lips_convert_qrel_file.py
@@ -3,19 +3,27 @@ import argparse
 
 def generate_sample_query_qrel_file_from_jsonl(input_qrel, output_qrel_txt, output_qrel_jsonl, query):
     output_file_list = []
+    jsonl_output_list = []
     with jsonlines.open(input_qrel, "r") as f:
         for line in f:
             if line["query"] == query_id:
                 #output_file_list.append(line)
                 output_file_list.append(line["query"]+" 0 "+line["document"]["entity"]+" "+str(line["relevance"])
+                output_dict= dict()
+                output_dict["relevance"] = 1
+                output_dict["query"] = line["query"]
+                item_dict = dict()
+                item_dict["entity"] = [line["document"]["entity"]]
+                output_dict["document"] = item_dict
+                jsonl_output_list.append(output_dict)
 
     with open(output_qrel_txt, "w", encoding='utf-8') as writer:
         for l in output_file_list:
             writer.write('%s\n' % l)
 
     with jsonlines.open(output_qrel_jsonl, mode='w') as f:
-            for a in output_file_list:
-                f.write(a)
+        for a in jsonl_output_list:
+            f.write(a)
 
 
 if __name__ == "__main__":
