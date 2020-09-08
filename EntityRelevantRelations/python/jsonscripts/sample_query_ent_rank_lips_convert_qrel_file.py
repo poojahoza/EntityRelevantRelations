551f0ecd88eae97c85f1c7dd5544073802f07521
poojahoza <oza.pooja.h@gmail.com>
2020-09-08 16:44:24 -0400

    sample_query_ent_rank_lips_convert_qrel_file.py - added

---
+++ EntityRelevantRelations/python/jsonscripts/sample_query_ent_rank_lips_convert_qrel_file.py
@@ -0,0 +1,28 @@
+import jsonlines
+import argparse
+
+def generate_sample_query_qrel_file_from_jsonl(input_qrel, output_qrel_txt, output_qrel_jsonl, query):
+    output_file_list = []
+    with jsonlines.open(input_qrel, "r") as f:
+        for line in f:
+            if line["query"] == query_id:
+                #output_file_list.append(line)
+                output_file_list.append(line["query"]+" 0 "+line["document"]["entity"]+" "+str(line["relevance"])
+
+    with open(output_qrel_txt, "w", encoding='utf-8') as writer:
+        for l in output_file_list:
+            writer.write('%s\n' % l)
+
+    with jsonlines.open(output_qrel_jsonl, mode='w') as f:
+            for a in output_file_list:
+                f.write(a)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Please provide qrel jsonl file loc, output text file location, output jsonl file location and query")
+    parser.add_argument('--q', help='entity qrel jsonl file location')
+    parser.add_argument('--txt', help='output qrel text file location')
+    parser.add_argument('--jsonl', help='output jsonl text file location')
+    parser.add_argument('--query', help='sample query')
+    args = parser.parse_args()
+    generate_sample_query_qrel_file_from_jsonl(args.q, args.txt, args.jsonl, args.query)
