187077a126488ea6bdeed8355fa280ec073f93b6
poojahoza <oza.pooja.h@gmail.com>
2020-09-28 12:08:28 -0400

    candidate_relation_annotations_analyze.py - updated to include relevancy

--- EntityRelevantRelations/python/jsonscripts/candidate_relation_annotations_analyze.py
+++ EntityRelevantRelations/python/jsonscripts/candidate_relation_annotations_analyze.py
@@ -24,12 +24,30 @@ def convert_dict_to_list(output_json):
         query_json['subject_relations_present'] = val['subject_relations_present']
         query_json['object_relations_present'] = val['object_relations_present']
         query_json['both_relations_present'] = val['both_relations_present']
+        query_json['relevant_subject_relations_present'] = val['relevant_subject_relations_present']
+        query_json['relevant_object_relations_present'] = val['relevant_object_relations_present']
+        query_json['relevant_both_relations_present'] = val['relevant_both_relations_present']
         output_list.append(query_json)
 
     return output_list
 
 
-def process_input_json_files(input_json_dir_loc):
+def process_qrel_files(input_qrel_file):
+    qrel_list = dict()
+    with open(input_qrel_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            data = line.split(" ")
+            query_id = data[0]
+            ent = data[2]
+            val = set()
+            if query_id in qrel_list:
+                val = qrel_list[query_id]
+            val.add(ent)
+            qrel_list[query_id] = val
+    #print(qrel_list)
+    return qrel_list
+
+def process_input_json_files(input_json_dir_loc, qrel_dict):
     output_json = dict()
     files = os.listdir(input_json_dir_loc)
     print(len(files))
@@ -44,6 +62,10 @@ def process_input_json_files(input_json_dir_loc):
                 subject_relations = 0  # how many relations have subject entities mapped
                 object_relations = 0   # how many relations have object entities mapped
                 both_relations = 0     # how many relations have both subject and object entities mapped
+                rel_sub_relations = 0  # how many relevant entities are present in subject if entities are present in both subject and object
+                rel_obj_relations = 0  # how many relevant entities are present in object if entities are present in both subject and object
+                rel_both_relations = 0 # how many relevant entities are present in both subject and object
+
                 for relation in item['relAnnotations']:
                     sub_ann = []
                     obj_ann = []
@@ -59,18 +81,35 @@ def process_input_json_files(input_json_dir_loc):
 
                     if len(sub_ann) > 0 and len(obj_ann) > 0:
                         both_relations = both_relations + 1
+                        qrel_rel_set = set(qrel_dict[query_id])
+                        sub_rel_set = set(sub_ann)
+                        obj_rel_set = set(obj_ann)
+                        sub_intersection_set_len = len(qrel_rel_set & sub_rel_set)
+                        obj_intersection_set_len = len(qrel_rel_set & obj_rel_set)
+                        if sub_intersection_set_len > 0:
+                            rel_sub_relations = rel_sub_relations + 1
+                        if obj_intersection_set_len > 0:
+                            rel_obj_relations = rel_obj_relations + 1
+                        if sub_intersection_set_len >0 and obj_intersection_set_len > 0:
+                            rel_both_relations = rel_both_relations + 1
 
                 if query_id in output_json:
                     output_json[query_id]['total_relations'] = output_json[query_id]['total_relations'] + total_relations
                     output_json[query_id]['subject_relations_present'] = output_json[query_id]['subject_relations_present'] + subject_relations
                     output_json[query_id]['object_relations_present'] = output_json[query_id]['object_relations_present'] + object_relations
                     output_json[query_id]['both_relations_present'] = output_json[query_id]['both_relations_present'] + both_relations
+                    output_json[query_id]['relevant_subject_relations_present'] = output_json[query_id]['relevant_subject_relations_present'] + rel_sub_relations
+                    output_json[query_id]['relevant_object_relations_present'] = output_json[query_id]['relevant_object_relations_present'] + rel_obj_relations
+                    output_json[query_id]['relevant_both_relations_present'] = output_json[query_id]['relevant_both_relations_present'] + rel_both_relations
                 else:
                     item_json = dict()
                     item_json['total_relations'] = total_relations
                     item_json['subject_relations_present'] = subject_relations
                     item_json['object_relations_present'] = object_relations
                     item_json['both_relations_present'] = both_relations
+                    item_json['relevant_subject_relations_present'] = rel_sub_relations
+                    item_json['relevant_object_relations_present'] = rel_obj_relations
+                    item_json['relevant_both_relations_present'] = rel_both_relations
                     output_json[query_id] = item_json
 
     return output_json
@@ -79,8 +118,10 @@ def process_input_json_files(input_json_dir_loc):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Please provide input JSON directory and output file path")
     parser.add_argument("--i", help="Input JSON folder location")
+    parser.add_argument("--q", help="Qrel file location")
     parser.add_argument("--o", help="Output csv file location")
     args = parser.parse_args()
-    json_dict = process_input_json_files(args.i)
+    qrel_dict = process_qrel_files(args.q)
+    json_dict = process_input_json_files(args.i, qrel_dict)
     output_list = convert_dict_to_list(json_dict)
     write_csv_file(args.o, output_list)
