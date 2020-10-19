fcafe537f569809148833f6c12aa135c04c98fe0
poojahoza <oza.pooja.h@gmail.com>
2020-10-19 12:07:38 -0400

    sample_query_ent_rank_lips_association_file_generator.py - updated

--- EntityRelevantRelations/python/jsonscripts/sample_query_ent_rank_lips_association_file_generator.py
+++ EntityRelevantRelations/python/jsonscripts/sample_query_ent_rank_lips_association_file_generator.py
@@ -33,6 +33,7 @@ def generate_assocations_file(input_folder, method_name, qrel_dict, query):
             input_json = json.load(f)
             for item in input_json:
                 if item['queryid'] == query:
+                    para_relational_neighbors = dict()
                     for rel in item['relAnnotations']:
                         sub_ann = []
                         obj_ann = []
@@ -44,29 +45,45 @@ def generate_assocations_file(input_folder, method_name, qrel_dict, query):
                         for s in sub_ann:
                             for o in obj_ann:
                                 if s in qrel_dict[item['queryid']] and o in qrel_dict[item['queryid']]:
-                                    ann_dict = dict()
-                                    ann_doc = dict()
-                                    ann_dict['query'] = item['queryid']
-                                    ann_dict['rank'] = 1
-                                    ann_dict['score'] = 1
-                                    ann_dict['method'] = method_name
-                                    ann_doc['paragraph'] = item['contextid']
-                                    ann_doc['neighbor'] = []
-                                    ann_doc['entity'] = [s, o]
-                                    ann_dict['document'] = ann_doc
-                                    output_list.append(ann_dict)
+                                    if s in para_relational_neighbors:
+                                        if o not in para_relational_neighbors[s]:
+                                            para_relational_neighbors[s].append(o)
+                                    else:
+                                        para_relational_neighbors[s] = [o]
                                 elif s not in qrel_dict[item['queryid']] and o not in qrel_dict[item['queryid']]:
-                                    ann_dict = dict()
-                                    ann_doc = dict()
-                                    ann_dict['query'] = item['queryid']
-                                    ann_dict['rank'] = 1
-                                    ann_dict['score'] = 1
-                                    ann_dict['method'] = method_name
-                                    ann_doc['paragraph'] = item['contextid']
-                                    ann_doc['neighbor'] = []
-                                    ann_doc['entity'] = [s, o]
-                                    ann_dict['document'] = ann_doc
-                                    output_list.append(ann_dict)
+                                    if s in para_relational_neighbors:
+                                        if o not in para_relational_neighbors[s]:
+                                            para_relational_neighbors[s].append(o)
+                                    else:
+                                        para_relational_neighbors[s] = [o]
+
+                        for obj in obj_ann:
+                            for sub in sub_ann:
+                                if obj in qrel_dict[item['queryid']] and sub in qrel_dict[item['queryid']]:
+                                    if obj in para_relational_neighbors:
+                                        if sub not in para_relational_neighbors[obj]:
+                                            para_relational_neighbors[obj].append(sub)
+                                    else:
+                                        para_relational_neighbors[obj] = [sub]
+                                elif obj not in qrel_dict[item['queryid']] and sub not in qrel_dict[item['queryid']]:
+                                    if obj in para_relational_neighbors:
+                                        if sub not in para_relational_neighbors[obj]:
+                                            para_relational_neighbors[obj].append(sub)
+                                        else:
+                                            para_relational_neighbors[obj] = [sub]
+
+                    for neghbr in para_relational_neighbors:
+                        ann_dict = dict()
+                        ann_doc = dict()
+                        ann_dict['query'] = item['queryid']
+                        ann_dict['rank'] = 1
+                        ann_dict['score'] = 1
+                        ann_dict['method'] = method_name
+                        ann_doc['paragraph'] = item['contextid']
+                        ann_doc['neighbor'] = para_relational_neighbors[neghbr]
+                        ann_doc['entity'] = neghbr
+                        ann_dict['document'] = ann_doc
+                        output_list.append(ann_dict)
                 print(counter)
                 counter = counter + 1
     return output_list
