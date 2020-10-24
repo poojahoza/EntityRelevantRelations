83cc3bee2db7bf4059648b643abc8728646a2fdd
poojahoza <oza.pooja.h@gmail.com>
2020-10-24 18:27:40 -0400

    create_query_heatmap.py - added new script to generate heatmap for a query

---
+++ EntityRelevantRelations/python/jsonscripts/create_query_heatmap.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 17:42:14 2020
+
+@author: poojaoza
+"""
+import json
+import os
+import argparse
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def read_multiple_json_files(folder_location):
+    files = os.listdir(folder_location)
+    content_json = []
+    try:
+        for file in files:
+            with open(folder_location+'/'+file, 'r', encoding='utf-8') as f:
+                content_json.extend(json.load(f))
+        return content_json
+    except Exception as e:
+        print(e)
+        return None
+    
+
+def create_relations_graph(input_json, query):
+
+    G = nx.Graph()
+    
+    for item in input_json:
+        if item['queryid'] == query:
+            for relation in item['relAnnotations']:
+                sub_ann = []
+                obj_ann = []
+    
+                for s_ann in relation['subjectAnnotations']:
+                    sub_ann.extend(s_ann['wiki_converted_id'])
+                for o_ann in relation['objectAnnotations']:
+                    obj_ann.extend(o_ann['wiki_converted_id'])
+    
+                for s in sub_ann:
+                    for o in obj_ann:
+                        if not G.has_edge(s, o):
+                            G.add_edge(s, o)
+    return G
+
+
+def create_heatmap(G):
+    fig, ax = plt.subplots(figsize=(12, 7))
+    title = query
+    plt.title(title, fontsize=10)
+    A = nx.to_numpy_matrix(G)
+    sns.heatmap(A, ax=ax)
+    plt.savefig('query1.png')
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Please provide relation annotation folder location,\
+                                     query and output folder location")
+    parser.add_argument("-a", "--annotations", help="Input relation annotations JSON folder location")
+    parser.add_argument("-q", "--query", help="Query to generate heatmap for")
+    parser.add_argument("-o", "--output", help="output png image file location")
+    args = parser.parse_args()
+    input_data = read_multiple_json_files(args.annotations)
+    query_graph = create_relations_graph(input_data, args.query)
+    create_heatmap(query_graph)
