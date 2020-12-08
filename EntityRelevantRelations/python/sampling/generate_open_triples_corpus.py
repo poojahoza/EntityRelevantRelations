956c15cf8bdbf03b879fe1c1ad786dc120244598
poojahoza <oza.pooja.h@gmail.com>
2020-12-08 08:18:53 -0500

    relation triples corpus generation - updated

--- EntityRelevantRelations/python/sampling/generate_open_triples_corpus.py
+++ EntityRelevantRelations/python/sampling/generate_open_triples_corpus.py
@@ -14,17 +14,25 @@ def unicode_text(text):
 
     return u' '.join(terms)
 
-def get_flat_relation_triples(input_json, output_file):
+def get_flat_relation_triples(input_json):
 
-    with codecs.open(output_file, 'w', encoding='utf-8') as f:
-        for item in input_json:
-            for relation in item['relAnnotations']:
-                relation_triple = u'{0} {1} {2}'.format(relation['subject'], relation['relation'], relation['object'])
-                triple = unicode_text(relation_triple)
-                f.write(triple + u'\n')
+    relation_corpus = ''
+    for item in input_json:
+        for relation in item['relAnnotations']:
+            relation_corpus = relation_corpus+' '+relation['subject']+' '+relation['relation']+' '+relation['object']
+    return [relation_corpus]
+
+    # with codecs.open(output_file, 'w', encoding='utf-8') as f:
+    #     for item in input_json:
+    #         for relation in item['relAnnotations']:
+    #             relation_triple = u'{0} {1} {2}'.format(relation['subject'], relation['relation'], relation['object'])
+    #             triple = unicode_text(relation_triple)
+    #             f.write(triple + u'\n')
 
 def generate_open_triples_corpus_wrapper(input_folder_loc, output_file_location):
     input_data = read_write_utils.read_multiple_json_files(input_folder_loc)
     print("read annotations file")
-    get_flat_relation_triples(input_data, output_file_location)
+    output_data = get_flat_relation_triples(input_data, output_file_location)
+    print("generated output string")
+    read_write_utils.write_text_file(output_file_location, output_data)
     print("generated corpus file")
